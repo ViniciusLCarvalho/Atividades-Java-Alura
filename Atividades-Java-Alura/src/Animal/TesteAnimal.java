c283bc232df379547b7dae643d4f1ce505d10523
Vinicius Carvalho <vinicius.carvalho1@outlook.com.br>
2025-08-01 11:19:10 -0300

    update exercise

--- Atividades-Java-Alura/src/Animal/TesteAnimal.java
+++ Atividades-Java-Alura/src/Animal/TesteAnimal.java
@@ -8,5 +8,13 @@ public class TesteAnimal {
         Gato gato = new Gato();
         gato.emitirSom();
         gato.arranharMoveis();
+
+        Animal animal = new Cachorro();
+
+        if (animal instanceof Cachorro) {
+            Cachorro dog = (Cachorro) animal;
+        } else {
+            System.out.println("O objeto não é um cachorro");
+        }
     }
 }
