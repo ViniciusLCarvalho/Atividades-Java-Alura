196f445a63460668e9a193dc579f426a2585efb6
Vinicius Carvalho <vinicius.carvalho1@outlook.com.br>
2025-11-04 18:18:31 -0300

    New example of reading a file using Scanner

---
+++ src/LeituraArquivoWithScanner/LeituraArquivo.java
@@ -0,0 +1,24 @@
+package LeituraArquivoWithScanner;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class LeituraArquivo {
+    public static void main(String[] args) {
+        try {
+            File arquivo = new File("arquivo.json");
+            Scanner scanner = new Scanner(arquivo);
+            
+            while (scanner.hasNextLine()) {
+                String linha = scanner.nextLine();
+                System.out.println(linha);
+            }
+            scanner.close();
+        } catch (FileNotFoundException e){
+            System.out.println("Arquivo não encontrado!");
+        }
+
+
+    }
+}
