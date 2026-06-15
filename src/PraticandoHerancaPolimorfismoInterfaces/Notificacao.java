7cc347e9f939c6f14a174a39eaa35b91d94a26ed
ViniciusCarvalho <vinicius.carvalho1@outlook.com.br>
2026-06-15 19:01:35 -0300

    Notification.java and PrincipalNotificacoes.java modiied

--- src/PraticandoHerancaPolimorfismoInterfaces/Notificacao.java
+++ src/PraticandoHerancaPolimorfismoInterfaces/Notificacao.java
@@ -9,5 +9,23 @@ public class Notificacao {
         this.mensagem = msg;
     }
 
+    public Notificacao(){
+        
+    }
+
     void enviar(){};
+
+    void enviarMensagem(String mensagem){
+        System.out.printf("Mensagem enviada para todos: %s\n", mensagem);
+    }
+
+    void enviarMensagem(String nome, String mensagem){
+        System.out.printf("Mensagem para %s: %s\n", nome, mensagem);
+    }
+
+    void enviarMensagem(String nome, String mensagem, int vezes){
+        for (int i = 0; i < vezes; i++) {
+            System.out.printf("Mensagem para %s: %s\n", nome, mensagem);
+        }
+    }
 }
--- src/PraticandoHerancaPolimorfismoInterfaces/PrincipalNotificacoes.java
+++ src/PraticandoHerancaPolimorfismoInterfaces/PrincipalNotificacoes.java
@@ -2,12 +2,18 @@ package PraticandoHerancaPolimorfismoInterfaces;
 
 public class PrincipalNotificacoes {
     public static void main(String[] args) {
-        Email e = new Email("cliente@exemplo.com", "Aproveite nossos descontos esta semana.", "Promoção Especial!");
-        Sms s = new Sms("(11) 98765-4321", "Sua fatura foi paga com sucesso.");
-        Push p = new Push("usuario_app", "Você tem uma nova mensagem não lida.", "Novidade!");
+        //Email e = new Email("cliente@exemplo.com", "Aproveite nossos descontos esta semana.", "Promoção Especial!");
+        //Sms s = new Sms("(11) 98765-4321", "Sua fatura foi paga com sucesso.");
+        //Push p = new Push("usuario_app", "Você tem uma nova mensagem não lida.", "Novidade!");
 
-        e.enviar();
-        s.enviar();
-        p.enviar();
+        //e.enviar();
+        //s.enviar();
+        //p.enviar();
+
+        Notificacao n = new Notificacao();
+
+        n.enviarMensagem("Olá!");
+        n.enviarMensagem("João", "Bom dia!");
+        n.enviarMensagem("Maria", "Atenção!", 2);
     }
 }
